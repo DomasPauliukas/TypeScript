91ac20e7f2d35736e0590c3581e2fa13ff2c578b
DomasPauliukas <Domas0319@gmail.com>
2025-01-30 19:55:44 +0200

    lesson-6

--- uzduotys/public/js/main.js
+++ uzduotys/public/js/main.js
@@ -1,14 +1,34 @@
 "use strict";
+const numbers = [1, 2, 3, 4, 5, 6, 7];
+const strings = ["pirmadienis", "antradienis", "trečiadienis", "ketvirtadienis", "penktadienis", "šeštadienis", "sekmadienis"];
+const booleans = [true, true, true, true, false];
 console.group('1. Parašykite funkciją, kuri grąžina pirmą masyvo elementą.');
 {
+    function getFirstElement(arr) {
+        return arr[0];
+    }
+    console.log({ Masyvas: numbers, 'Pirmas elementas': getFirstElement(numbers) });
 }
 console.groupEnd();
 console.group('2. Parašykite funkciją, kuri grąžina paskutinį masyvo elementą.');
 {
+    const getFirstElement = (arr) => {
+        return arr[arr.length - 1];
+    };
+    console.log(getFirstElement(strings));
+    console.log(getFirstElement(numbers));
+    console.log(getFirstElement(booleans));
 }
 console.groupEnd();
 console.group('3. Parašykite funkciją, kuri grąžina vienarūšių primityvių reikšmių masyvo kopiją');
 {
+    const getCopiedArray = (arr) => {
+        let copy = [...arr];
+        return copy;
+    };
+    console.log(getCopiedArray(numbers));
+    console.log(getCopiedArray(strings));
+    console.log(getCopiedArray(booleans));
 }
 console.groupEnd();
 console.group('4. Parašykite funkciją,  kuri pirmu parametru priima string | number | boolen, grąžina to tipo masyvą su perduota reikšme tiek kartų, kiek nurodyta antru parametru');
@@ -17,10 +37,39 @@ console.group('4. Parašykite funkciją,  kuri pirmu parametru priima string | n
 console.groupEnd();
 console.group('5. Parašykite funkciją, kuri sujungia tokių pat tipų masyvus į vieną masyvą');
 {
+    const getArraysMerged = (arr1, arr2) => {
+        return [...arr1, ...arr2];
+    };
+    const numberArr = [[1, 2, 3], [4, 5, 6]];
+    const stringArr = [['labas', 'sveiki'], ['ate', 'iki']];
+    console.log(getArraysMerged(...numberArr));
+    console.log(getArraysMerged(...stringArr));
 }
 console.groupEnd();
 console.group('6. Parašykite funkciją, kuri priimtų bet kokią reikšmę ir grąžintų objektą su savybėmis-funkcijomis "setValue" - reikšmei nustatyti ir "getValue" tai reikšmei nustatyti. Funkcijai perduota reikšmė neturi būti pasiekiama tiesiogiai.');
 {
+    function valueFunction(initialValue) {
+        let value = initialValue;
+        return {
+            setValue: (newValue) => { value = newValue; },
+            getValue: () => value,
+        };
+    }
+    const value1 = 5;
+    const value2 = ['hey', 'hello'];
+    const value3 = { name: 'Domas', surname: 'Pauliukas' };
+    const obj1 = valueFunction(value1);
+    const obj2 = valueFunction(value2);
+    const obj3 = valueFunction(value3);
+    console.log(obj1.getValue());
+    console.log(obj2.getValue());
+    console.log(obj3.getValue());
+    obj1.setValue(9);
+    obj2.setValue(['Bye Bye']);
+    obj3.setValue({ name: 'Petras', surname: 'Petraitis' });
+    console.log(obj1.getValue());
+    console.log(obj2.getValue());
+    console.log(obj3.getValue());
 }
 console.groupEnd();
 console.group(`
--- uzduotys/src/main.ts
+++ uzduotys/src/main.ts
@@ -7,22 +7,47 @@ type PrimitiveType = string | number | boolean;
   7 užduotis, skirta savarankiškai išmokti patikrinti tipus:
   https://www.typescriptlang.org/docs/handbook/advanced-types.html#user-defined-type-guards
 */
+const numbers: number[] = [1, 2, 3, 4, 5, 6, 7];
+const strings: string[] = ["pirmadienis", "antradienis", "trečiadienis", "ketvirtadienis", "penktadienis", "šeštadienis", "sekmadienis"];
+const booleans: boolean[] = [true, true, true, true, false];
+
 
 console.group('1. Parašykite funkciją, kuri grąžina pirmą masyvo elementą.');
 {
-  // Sprendimas ir rezultatų spausdinimas
+  function getFirstElement <Type>(arr: Type[]): Type {
+    return arr[0]
+  }
+
+  console.log({Masyvas: numbers, 'Pirmas elementas': getFirstElement(numbers)})
+
 }
 console.groupEnd();
 
 console.group('2. Parašykite funkciją, kuri grąžina paskutinį masyvo elementą.');
 {
-  // Sprendimas ir rezultatų spausdinimas
+  const getFirstElement = <Type>(arr: Type[]) => {
+    return arr[arr.length - 1]
+  }
+
+  console.log(getFirstElement(strings))
+  console.log(getFirstElement(numbers))
+  console.log(getFirstElement(booleans))
+
 }
 console.groupEnd();
 
 console.group('3. Parašykite funkciją, kuri grąžina vienarūšių primityvių reikšmių masyvo kopiją');
 {
-  // Sprendimas ir rezultatų spausdinimas
+  const getCopiedArray = <Type extends PrimitiveType>(arr: Type[]) => {
+    let copy = [...arr]
+    return copy
+  }
+
+  console.log(getCopiedArray(numbers))
+  console.log(getCopiedArray(strings))
+  console.log(getCopiedArray(booleans))
+
+
 }
 console.groupEnd();
 
@@ -37,13 +62,62 @@ console.groupEnd();
 
 console.group('5. Parašykite funkciją, kuri sujungia tokių pat tipų masyvus į vieną masyvą');
 {
-  // Sprendimas ir rezultatų spausdinimas
+  type TwoArrays<Type> = [Type[], Type[]]
+
+  const getArraysMerged = <Type>(arr1: Type[], arr2: Type[]) => {
+    return [...arr1,...arr2]
+  }
+
+  const numberArr: TwoArrays <number> = [[1,2,3],[4,5,6]]
+  const stringArr: TwoArrays <string> = [['labas', 'sveiki'],['ate', 'iki']]
+
+  console.log(getArraysMerged(...numberArr))
+  console.log(getArraysMerged(...stringArr))
+
+
 }
 console.groupEnd();
 
 console.group('6. Parašykite funkciją, kuri priimtų bet kokią reikšmę ir grąžintų objektą su savybėmis-funkcijomis "setValue" - reikšmei nustatyti ir "getValue" tai reikšmei nustatyti. Funkcijai perduota reikšmė neturi būti pasiekiama tiesiogiai.');
 {
-  // Sprendimas ir rezultatų spausdinimas
+  type ValueType<Type> = {
+    setValue: (newValue: Type) => void,
+    getValue: () => Type,
+  }
+
+
+  function valueFunction <Type>(initialValue: Type): ValueType<Type> {
+    let value = initialValue
+
+    return {
+      setValue: (newValue) => {value = newValue},
+      getValue: () => value,
+    }
+  }
+
+const value1: number = 5
+const value2: Array<string> = ['hey', 'hello']
+const value3: {name: string, surname: string} = {name: 'Domas', surname: 'Pauliukas'}
+
+const obj1 = valueFunction(value1)
+const obj2 = valueFunction(value2)
+const obj3 = valueFunction(value3)
+
+
+console.log(obj1.getValue())
+console.log(obj2.getValue())
+console.log(obj3.getValue())
+
+obj1.setValue(9)
+obj2.setValue(['Bye Bye'])
+obj3.setValue({name: 'Petras', surname: 'Petraitis'})
+
+console.log(obj1.getValue())
+console.log(obj2.getValue())
+console.log(obj3.getValue())
+
+
+
 }
 console.groupEnd();
 
@@ -69,6 +143,12 @@ console.group(`
     avgMonthlyPay: number,
   };
 
+  type GroupedPeople = {
+    people: Person[],
+    students: Student[],
+    workers: Worker[],
+  }
+
   const people: (Person | Student | Worker)[] = [
     { name: 'Atstovė', surname: 'Galtokaitė', university: 'VU', course: 2 },
     { name: 'Kurpius', surname: 'Medainis' },
@@ -79,4 +159,10 @@ console.group(`
     { name: 'Šidelė', surname: 'Gyslovienė', avgMonthlyPay: 1500 },
     { name: 'Užuodauskas', surname: 'Perrašimauskas', university: 'VGTU', course: 1 },
   ];
+
+  const isStudent = (people) => {
+    
+  }
+
+
 }
